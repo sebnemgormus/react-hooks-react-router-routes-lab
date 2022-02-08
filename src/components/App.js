3244aace6da8fee1408f57156dc2d57b3cc686da
Sebnem <sebnemgormus@gmail.com>
2022-02-08 21:33:41 +0300

    commit tree 991344aeb6e0c91e2e9e8ece554cd2a4cd59c08d

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,21 @@ import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (<BrowserRouter>
+    <div className="App">
+      <NavBar />
+    </div> 
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/movies" component={Movies}/>
+      <Route path="/directors" component={Directors}/>
+      <Route path="/actors" component={Actors}/>
+    </Switch>
+  </BrowserRouter>
+  )
 }
 
 export default App;
